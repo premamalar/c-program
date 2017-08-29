6df6a6fcd0ee92587484f8457299fa5ab8a6a0bd
premamalar <31436397+premamalar@users.noreply.github.com>
2017-08-29 15:35:14 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,3 +3,13 @@ int main()
 {
 char ch;
 printf("enter the alphabet:",ch);
+  if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='O'||ch='I'||ch=='U')
+  {
+ printf("it is a vowel",ch);
+  }
+  else
+  {
+    printf("it is a constant",ch);
+  }
+  return 0;
+}
