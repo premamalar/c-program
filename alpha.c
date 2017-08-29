62cd3743488c1786370a02fbf38efa300b6b8fce
premamalar <31436397+premamalar@users.noreply.github.com>
2017-08-29 15:39:40 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the alphabet:",ch);
+  if(ch>='a' && ch<='z' ||ch='A' && ch='Z')
+  {
+ printf("it is a alphabet",ch);
+  }
+  else
+  {
+    printf("it is not a alphabet",ch);
+  }
+  return 0;
+}
