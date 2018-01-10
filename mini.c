00341e212bec235c0d8c2e898caa44ff4e206b72
premamalar <31436397+premamalar@users.noreply.github.com>
2018-01-10 16:50:06 +0530

    Update mini.c

--- mini.c
+++ mini.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+void main()
+{
+  int x,z;
+  printf("enter the value of x:");
+  scanf("%d",x);
+  int i;
+  for(i=0;i<=x;i++)
+  {
+   z= min(x);
+  }
+  printf("result:%d",z);
+}
