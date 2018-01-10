9a1aef98ac6ba29d40590bc3cf1e6409f8e54d1b
premamalar <31436397+premamalar@users.noreply.github.com>
2018-01-10 16:12:40 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+int b[];
+printf("enter the integer value:");
+scanf("%d",&a);
+for(a=1;a<=n;a++)
+{
+print("print the maximium value of a",a);
+}
+return 0;
+}
