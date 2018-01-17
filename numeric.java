92ddf20307d41a33bd42b3d73db9f02554d8349d
premamalar <31436397+premamalar@users.noreply.github.com>
2018-01-17 15:38:47 +0530

    Create numeric.java

---
+++ numeric.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public static void main(String args[])
+{
+int num;
+system.out.println("enter the numeric values:",num);
+if(num != 'a'||'z')
+{
+system.out.println("numeric");
+}
+else
+{
+system.out.println("not numeric");
+}
+}
