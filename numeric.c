ff4511a2e41665a0fbb85ec0f18b13634f76789c
premamalar <31436397+premamalar@users.noreply.github.com>
2018-01-17 15:42:10 +0530

    Update numeric.c

--- numeric.c
+++ numeric.c
@@ -2,13 +2,13 @@
 void main()
 {
 int num;
-system.out.println("enter the numeric values:",num);
+printf("enter the numeric values:",num);
 if(num != 'a'||'z')
 {
-system.out.println("numeric");
+  printf("numeric");
 }
 else
 {
-system.out.println("not numeric");
+printf("not numeric");
 }
 }
