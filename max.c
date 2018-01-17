4e5973001bbb58439b1f96def1a03e01b66689ec
premamalar <31436397+premamalar@users.noreply.github.com>
2018-01-17 15:53:49 +0530

    Update max.c

--- max.c
+++ max.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-int a,b,i;
+int a,b,i,z,n=10000;
 printf("enter the integer value:");
-scanf("%d",&a,b);
-for(i=1;i<=N;i++)
+scanf("%d%d",&a,&b);
+for(i=1;i<=n;i++)
 {
-  z=max(a,b);
-print("print the maximium value of a,b",z);
+  if(a<=b)
+  {
+ z=(a,b);
+printf("print the maximium value of a and b");
+scanf("%d",&z);  }
 }
 return 0;
 }
