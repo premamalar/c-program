c683a65fd4b8d09e8e8fb4ed6f8f06d6c3ce3bd5
premamalar <31436397+premamalar@users.noreply.github.com>
2018-01-17 16:27:05 +0530

    Update ascsort.c

--- ascsort.c
+++ ascsort.c
@@ -3,9 +3,10 @@ int main()
 {
   int a[5]={'2','1','3'};
   int i;
-  for(i=0;i<=5;i++)
-  {
-printf("sorted array");
-scanf("%d",&a[i]);
+  for(i=0;i<=a[5];i++)
+  { 
+   if(sort=a[i])
+printf("sorted array for 2,1,3 %d",a[i]);
+  }
 return 0;
 }
