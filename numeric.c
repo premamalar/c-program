fcb4c9d325666de106d24fa6fc0e59f2144e14f0
premamalar <31436397+premamalar@users.noreply.github.com>
2018-01-17 15:40:48 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int num;
+system.out.println("enter the numeric values:",num);
+if(num != 'a'||'z')
+{
+system.out.println("numeric");
+}
+else
+{
+system.out.println("not numeric");
+}
+}
