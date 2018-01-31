24fe41e9e7363ab27c6c6e77cddc6037c9dd84d0
premamalar <31436397+premamalar@users.noreply.github.com>
2018-01-31 16:35:34 +0530

    Create pstr.c

---
+++ pstr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,i;
+	char str[30];
+	scanf("%d",&a);
+	scanf("%s",&str);
+	for(i=1;i<=a;i++)
+	{
+		printf("%s",str);
+	}
+	// your code goes here
+	return 0;
+}
