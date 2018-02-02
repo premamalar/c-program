3a341ff50a8aab7105ae3a41f1f1ac31c80345d6
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-02 15:52:10 +0530

    Create cmp.c

---
+++ cmp.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,j;
+char c[10],d[20];
+printf("enter the first string:);
+scanf("%c",&c);
+printf("enter the second string:");
+scanf("%d",&d);
+a=strlen(c[i]);
+b=strlen(d[j]);
+if(c[i]<=d[j])
+{
+printf("greater string");
+}
+else
+{
+printf("equal string");
+}
+return 0;
+}
