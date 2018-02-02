61b3b167eac964f6a895e3066e66ae40f601c6b3
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-02 16:31:22 +0530

    Create cat.c

---
+++ cat.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+void main()
+{
+char c[10],d[20],e[50];
+printf("enter the first string:");
+scanf("%c",&c[10]);
+printf("enter the second string:");
+scanf("%c",&d[20]);
+e[50]=(c[10],d[20]);
+printf("%C",e[50]);
+}
