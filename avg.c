3e8a356e0c76661c6fe90a03ed0f8b3418aca21b
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-05 16:01:12 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a=5,temp,temp1,temp2,temp3,temp4,z;
+temp=1;
+temp1=temp+1;
+temp2=temp1+1;
+temp3=temp2+1;
+temp4=temp3+1;
+z=(temp+temp1+temp2+temp3+temp4);
+printf("value%d",z);
+printf("average:%d",z/5);
+	// your code goes here
+return 0;
+}
