aa6db977afcfa64f15603d74d72f78bfae5e37e7
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-05 15:49:27 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("enter a value");
+	scanf("%d",&a);
+if(a<=10)
+{
+	printf("YES");
+}
+else
+{
+	printf("NO");
+}
+	// your code goes here
+return 0;
+}
