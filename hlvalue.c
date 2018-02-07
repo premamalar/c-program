0d9da9d1fcc568162c5ddfa318977d8eccc90ae6
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-07 15:52:30 +0530

    Create hlvalue.c

---
+++ hlvalue.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[10],i,j;
+	printf("enter the numbers");
+	scanf("%d",&a[10]);
+            for(i=0;i<=a[10];i++)
+            {
+		printf("lowest value :%d",a[10]);
+	}
+	for(j=1;j<=a[10];j++)
+	{
+        
+            if(a[10]<=10000)
+            {
+            	 printf("greatest value:%d",a[10]);
+            }
+            }
+	
+	// your code goes here
+return 0;
+}
