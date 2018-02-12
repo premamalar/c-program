7501eeadcb87daf855433446e5519f1397a9568e
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-12 16:23:57 +0530

    Create p58.c

---
+++ p58.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int x,y;
+	printf("enter x and y values:");
+	scanf("%d%d",&x,&y);
+	x=x^y;
+	x=x^y;
+	x=x^y;
+printf("%d%d",y,x);
+	// your code goes here
+	return 0;
+}
