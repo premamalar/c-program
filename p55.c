a2e40633086908a1f23fafb1e9da5a2bf5c6623c
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-12 15:56:41 +0530

    Create p55.c

---
+++ p55.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int m,n,z;
+	printf("enter m and n values:");
+	scanf("%d%d",&m,&n);
+	z=m*n;
+	printf("%d",z);
+	if(z%=2)
+	{
+		printf("ODD");
+}
+else
+{
+	printf("EVEN");
+	}
+	// your code goes here
+	return 0;
+}
