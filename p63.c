58f053bd1d949228723d3e098d12f5191ac36fcd
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-21 15:39:55 +0530

    Create p63.c

---
+++ p63.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c[10];
+int count=1,i;
+clrscr();
+printf("enter the character");
+for(i=1;i<=5;i++)
+{
+scanf("%c",&c[i]);
+}
+printf("the value is");
+if(count<=10)
+{
+printf("%d",c[i]);
+}
+else
+{
+printf("%d",c[i]);
+}
+getch();
+}
