f06629297689a25e88ac06d7271cb312d0faf4f5
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-26 15:43:29 +0530

    Create p71.c

---
+++ p71.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char c;
+int r,t;
+printf("enter the elements:");
+scanf("%s",&c);
+t=c;
+while(t!='\0')
+{
+r=r*10;
+r=r+t%10;
+t=t/10;
+}
+if(c==r)
+{
+printf("palindrome");
+}
+else
+{
+printf("not palindrome");
+}
+return 0;
+}
