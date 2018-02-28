a9ee616524d802b875b290e7c317ea6d00bf3cfd
premamalar <31436397+premamalar@users.noreply.github.com>
2018-02-28 16:13:55 +0530

    Create p81.c

---
+++ p81.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ clrscr();
+ printf("enter  a nd b value");
+ scanf("%d%d",&a,&b);
+ c=b-a;
+printf("difference %d",c);
+getch();
+}
