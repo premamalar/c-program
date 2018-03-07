064eeba09ec0460ce1f785c0af77933082d09f63
premamalar <31436397+premamalar@users.noreply.github.com>
2018-03-07 15:53:32 +0530

    Create p93.c

---
+++ p93.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int celsius,kelvin;
+clrscr();
+printf("enter celsius temperature:");
+scanf("%d",&celsius);
+kelvin=celsius+273;
+printf("kelvin temperature is%d",kelvin);
+getch();
+}
