42b0a7644342c02770f936c15102f9fe1a60b8e5
premamalar <31436397+premamalar@users.noreply.github.com>
2018-03-07 16:54:38 +0530

    Create p104.c

---
+++ p104.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int N,K,sum;
+clrscr();
+printf("enter N,K");
+scanf("%d%d",&N,&K);
+sum=pow(N,K);
+printf("%d",sum);
+getch();
+}
