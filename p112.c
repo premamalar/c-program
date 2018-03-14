e6a83be954cd0b21c990aa01fb358873ef672de6
premamalar <31436397+premamalar@users.noreply.github.com>
2018-03-14 16:29:28 +0530

    Create p112.c

---
+++ p112.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n,k;
+clrscr();
+printf("enter  n :");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&n);
+}
+scanf("%d",&k);
+for(j=1;j<k;j++)
+{
+if(k!=0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+getch();
+}
