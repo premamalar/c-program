67d0d5fec08435a649e583bb92721cbd57f9ad06
premamalar <31436397+premamalar@users.noreply.github.com>
2018-03-21 16:48:55 +0530

    Update player14.c

--- player14.c
+++ player14.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 #include<string.h>
 int main(void) {
-	char str[25];
-	int i,x,y;
-	printf("enter the string");
-	scanf("%s",&str);
-	y=strlen(str);
-	while(y!=0)
-            {
-		for(i=0;i<=y;i++)
-		{
-			x=str[i];
-		}
-	}
-	printf("%s",str);
-	for(i=0;i<x;i++)
+	char a[100];
+	int m,i;
+	scanf("%s",a);
+	m=strlen(a);
+	for(i=m-1;i>=0;i--)
 	{
-		x=str[i];
-		x%=10;
+		if(a[i]!='a' && a[i]!='e' && a[i]!='U' &&a[i]!='i' &&a[i]!='o' &&a[i]!='u' &&a[i]!='A' &&a[i]!='E' &&a[i]!='I' &&a[i]!='O' )
+		printf("%c",a[i]);
 	}
-	// your code goes here
 	return 0;
 }
