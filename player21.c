68372709337042ba48e145c30dcab09a364b11f0
premamalar <31436397+premamalar@users.noreply.github.com>
2018-03-28 16:43:33 +0530

    Create player21.c

---
+++ player21.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) {
+	int x[20],y[20],z[20],i;
+	printf("enter x");
+	for(i=1;i<=2;i++)
+	{
+		scanf("%d",&x);
+	}
+	printf("enter y");
+	for(i=1;i<=2;i++)
+	{
+		scanf("%d",&y);
+	}
+	printf("enter z");
+	for(i=1;i<=2;i++)
+	{
+		scanf("%d",&z);
+	}
+		printf("YES");
+	// your code goes here
+	return 0;
+}
