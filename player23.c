a0171560d03b053fe988021637787a6b9c3c9178
premamalar <31436397+premamalar@users.noreply.github.com>
2018-03-28 15:52:43 +0530

    Create player23.c

---
+++ player23.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+int x[25],y[25],i,j;
+printf("enter x :");
+for(i=0;i<=x;i++)
+{
+scanf("%d",&x);
+}
+printf("enter y:");
+for(j=0;j<=y;j++)
+{
+scanf("%d",&y);
+}
+printf("%d%d",x[3],y[0]);
+
+	// your code goes here
+return 0;
+}
